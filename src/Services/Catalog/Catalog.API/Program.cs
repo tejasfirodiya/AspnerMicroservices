1e241d1b93187236987ee25134e214edf734630b
Tejas Firodiya <tejas.firodiya@waiin.com>
2023-10-22 21:30:59 +0530

    Commit changes for catalog API project with mongo db

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,38 @@
+using Catalog.API.Data;
+using Catalog.API.Repositories;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+// Configure your services here
+//builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog Api", Version = "v1" });
+});
+
+builder.Services.AddScoped<ICatalogContext, CatalogContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ src/Services/Catalog/Catalog.API/Startup.cs
@@ -0,0 +1,56 @@
+﻿//using Catalog.API.Data;
+//using Catalog.API.Repositories;
+//using Microsoft.AspNetCore.Builder;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.OpenApi.Models;
+
+//namespace Catalog.API
+//{
+//    public class Startup
+//    {
+//        public Startup(IConfiguration configuration)
+//        {
+//            Configuration = configuration;
+//        }
+
+//        public IConfiguration Configuration { get; }
+
+//        public void ConfigureServices(IServiceCollection services)
+//        {
+//            // Configure your services here
+//            services.AddControllers();
+//            services.AddSwaggerGen(c =>
+//            {
+//                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog Api", Version = "v1" });
+//            });
+
+//            services.AddScoped<ICatalogContext, CatalogContext>();
+//            services.AddScoped<IProductRepository, ProductRepository>();
+//        }
+
+//        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//        {
+//            // Configure your middleware and routing here
+//            if (env.IsDevelopment())
+//            {
+//                app.UseDeveloperExceptionPage();
+//                app.UseSwagger();
+//                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1"));
+//            }
+//            else
+//            {
+//                app.UseExceptionHandler("/Home/Error");
+//                app.UseHsts();
+//            }
+
+//            app.UseRouting();
+
+//            app.UseAuthorization();
+
+//            app.UseEndpoints(endpoints =>
+//            {
+//                endpoints.MapControllers();
+//            });
+//        }
+//    }
+//}
